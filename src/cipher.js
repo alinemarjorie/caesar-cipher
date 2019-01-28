a45114ac57877b0fafa16371a41066f7e58dd4cd
alinemarjorie <aline.marjorie.af@gmail.com>
2019-01-28 12:36:39 -0200

    Criei a função de criptografar letras maiúsculas

--- src/cipher.js
+++ src/cipher.js
@@ -1,3 +1,22 @@
-window.cipher = {
+//window.cipher = {
   // ... 
-};
+//};
+
+function button(){
+let answer = document.getElementById("answer").value;
+let  offset = document.getElementById("offset").value;
+encode(answer, parseInt(offset));
+}
+
+function encode(answer,offset){
+  let cipherMessage = [];
+  for (let i = 0; i < answer.length; i++) {
+    let asciiCode = answer[i].charCodeAt();  
+    if (asciiCode >= 65 && asciiCode <= 90) {
+      let letter = ((asciiCode - 65 + offset) % 26) + 65;
+      cipherMessage.push(String.fromCharCode(letter));
+    }
+  }
+console.log (cipherMessage);
+
+}
