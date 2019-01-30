88f1f061cd3a38d9d2ef466707f2b54b35015c83
alinemarjorie <aline.marjorie.af@gmail.com>
2019-01-30 13:25:23 -0200

    Funções de codificar e descodificar criadas e funcionando, considerando espaço

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,4 @@
-//window.cipher = {
-  // ... 
-//};
-
-function button(){
+function buttonC(){
 let answer = document.getElementById("answer").value;
 let  offset = document.getElementById("offset").value;
 encode(answer, parseInt(offset));
@@ -13,10 +9,43 @@ function encode(answer,offset){
   for (let i = 0; i < answer.length; i++) {
     let asciiCode = answer[i].charCodeAt();  
     if (asciiCode >= 65 && asciiCode <= 90) {
-      let letter = ((asciiCode - 65 + offset) % 26) + 65;
-      cipherMessage.push(String.fromCharCode(letter));
+      let letterMa = ((asciiCode - 65 + offset) % 26) + 65;
+      cipherMessage.push(String.fromCharCode(letterMa));
+    }
+    else if (asciiCode >= 97 && asciiCode <= 122){
+      let letterMi = ((asciiCode - 97 + offset) % 26) + 97;
+      cipherMessage.push(String.fromCharCode(letterMi));
+    }
+    else if (asciiCode === 32){
+      let space = 32;
+      cipherMessage.push(String.fromCharCode(space));
     }
   }
-console.log (cipherMessage);
+document.getElementById("resultCode").innerHTML = cipherMessage.join("");
+}
 
+function buttonD(){
+  let answer = document.getElementById("answer").value;
+  let  offset = document.getElementById("offset").value;
+  decode(answer, parseInt(offset));
+  }
+
+function decode(answer,offset){
+  let cipherMessage = [];
+  for (let i = 0; i < answer.length; i++) {
+    let asciiCode = answer[i].charCodeAt();
+    if (asciiCode >= 65 && asciiCode <= 90) {
+      let letterMa = (asciiCode - 65 - (offset %26) + 26) %26 + 65;
+      cipherMessage.push(String.fromCharCode(letterMa));
+    }
+    else if (asciiCode >= 97 && asciiCode <= 122){
+      let letterMi = (asciiCode - 97 - (offset %26) + 26) %26 + 97;
+      cipherMessage.push(String.fromCharCode(letterMi));
+    }
+    else if (asciiCode === 32){
+      let space = 32;
+      cipherMessage.push(String.fromCharCode(space));
+    }
+  }
+document.getElementById("resultCode").innerHTML = cipherMessage.join("");
 }
