b9a2b55f385fe15085d6f8becb0d40b43f642854
alinemarjorie <aline.marjorie.af@gmail.com>
2019-01-31 21:00:15 -0200

    resultado voltou para a mesma tela, mas está considerando todos os caracteres (hacker edition)

--- src/cipher.js
+++ src/cipher.js
@@ -1,7 +1,7 @@
-function buttonC(){
-let answer = document.getElementById("answer").value;
-let  offset = document.getElementById("offset").value;
-encode(answer, parseInt(offset));
+function buttonC() {
+  let answer = document.getElementById("answer").value;
+  let  offset = document.getElementById("offset").value;
+  encode(answer, parseInt(offset));
 }
 
 function encode(answer,offset){
@@ -11,46 +11,36 @@ function encode(answer,offset){
     if (asciiCode >= 65 && asciiCode <= 90) {
       let letterMa = ((asciiCode - 65 + offset) % 26) + 65;
       cipherMessage.push(String.fromCharCode(letterMa));
-    }
-    else if (asciiCode >= 97 && asciiCode <= 122){
+    } else if (asciiCode >= 97 && asciiCode <= 122){
       let letterMi = ((asciiCode - 97 + offset) % 26) + 97;
       cipherMessage.push(String.fromCharCode(letterMi));
-    }
-    else if (asciiCode === 32){
-      let space = 32;
-      cipherMessage.push(String.fromCharCode(space));
+    } else {
+      cipherMessage.push(String.fromCharCode(asciiCode));
     }
   }
-document.getElementById('final').style.display = 'block';
-document.getElementById('inicio').style.display = 'none';
 document.getElementById("resultCode").innerHTML = cipherMessage.join("");
 }
 
-function buttonD(){
+function buttonD() {
   let answer = document.getElementById("answer").value;
   let  offset = document.getElementById("offset").value;
   decode(answer, parseInt(offset));
-  }
+ }
 
-function decode(answer,offset){
+function decode(answer,offset) {
   let cipherMessage = [];
   for (let i = 0; i < answer.length; i++) {
     let asciiCode = answer[i].charCodeAt();
     if (asciiCode >= 65 && asciiCode <= 90) {
       let letterMa = (asciiCode - 65 - (offset %26) + 26) %26 + 65;
       cipherMessage.push(String.fromCharCode(letterMa));
-    }
-    else if (asciiCode >= 97 && asciiCode <= 122){
+    } else if (asciiCode >= 97 && asciiCode <= 122){
       let letterMi = (asciiCode - 97 - (offset %26) + 26) %26 + 97;
       cipherMessage.push(String.fromCharCode(letterMi));
-    }
-    else if (asciiCode === 32){
-      let space = 32;
-      cipherMessage.push(String.fromCharCode(space));
+    } else {
+      cipherMessage.push(String.fromCharCode(asciiCode));
     }
   }
 
-document.getElementById('final').style.display = 'block';
-document.getElementById('inicio').style.display = 'none';
 document.getElementById("resultCode").innerHTML = cipherMessage.join("");
 }
